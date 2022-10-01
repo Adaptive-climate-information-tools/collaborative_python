25375c5a91b4276a47127ad912ea7459eb7131bf
ellendyer <64270734+ellendyer@users.noreply.github.com>
2022-10-01 13:19:34 +0100

    new code to read tsv

---
+++ code/workshop_test_shape.py
@@ -0,0 +1,255 @@
+#Step 1: Start with import statements (this tells python which packages 
+#you will actually use in your code and names 
+#them, so we will call xarray : xr)
+import xarray as xr
+from matplotlib import pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature
+from cartopy.io.shapereader import Reader
+from cartopy.feature import ShapelyFeature
+from shapely.geometry import mapping
+import geopandas as gpd
+
+#%%
+#Step 2: Read in our data from a netcdf file of rainfall over Ethiopia 
+#(edit the path to the file correctly depending on where you saved it) 
+#and select the variable ‘precip’. Now we have a data array called da and
+#we can print it to see what we have:
+ds = xr.open_dataset("./workshop_chirps.nc")
+da = ds["precip"]
+# print data array
+print(da)
+# print attributes in file metadata
+print(da.attrs)
+# print a list of array dimensions
+print(da.dims)
+# print a list of array coordinates
+print(da.coords)
+# print time and lon dim labels
+print(da.time)
+print(da.lon)
+
+
+#%%
+#Step 3 : Let’s make a quick plot of one month of data. 
+#To do this we will select one date using the .sel function 
+#and using the coordinate time and its labels. In xarray you can 
+#use date strings to select time steps (use the coordinate format 
+#that we saw when printing da.time above). If you instead wanted to 
+#choose the first time element you would use .isel which lets you select 
+#using normal array indices, so the first month would be .isel(time=0).
+
+#use coordinate names
+da.sel(time='2018-07-01').plot()
+plt.show()
+plt.clf()
+
+#%%
+#Step 4 : Let’s select and modify data. First we will make a timeseries by taking 
+#an average over the dimensions lat and lon. Remember to use the dimension names 
+#that we found in printing da.dims above. We can then select a smaller time range 
+#for this time series by using .sel again but this time by takin a slice. 
+#If you are selecting a single point you either need to know the exact coordinates 
+#or you need to ask xarray to select the ‘nearest’ point.
+
+#make a timeseries plot by taking a regional average
+ts = da.mean(dim=('lat','lon'))
+print(ts)
+ts.plot()
+plt.show()
+plt.clf()
+
+#select a time slice from ts timeseries (you can use date strings
+#with xarray)the .sel slice method chooses the nearest point so 
+#you don't need to know exact bounds
+ts = ts.sel(time=slice('2015-01-01','2017-12-31'))
+ts.plot()
+plt.show()
+plt.clf()
+
+#if you use .sel without slice then you need to say 
+#that you want the nearest point
+print(da.sel(lat=3.333,lon=40.111,method='nearest'))
+
+#%%
+#Step 5 : Xarray has use some of the useful grouping functions like 
+#groupby (makes bins), rolling (can do rolling averages), 
+#resample (can change time frequencies) and more!
+
+#groupby just makes bins but doesn’t alter your data array  
+print(da.groupby("time.season"))
+
+#if you want to alter it you need to act on it and here we do 
+#this by taking a time mean with .mean(‘time’)
+#calculate long term seasonal means 
+#(now 'time' has been replaced by 'season')
+print(da.groupby("time.season").mean('time'))
+#calculate a timeseries of annual averages 
+#(now 'time' has been replaced by 'year')
+print(da.groupby("time.year").mean('time'))
+
+# create new data arrays with these time means 
+seas_mean = da.groupby("time.season").mean('time')
+ann_mean = da.groupby("time.year").mean('time')
+
+#calculate long term seasonal anomalies from the long term annual mean
+#you can do simple matrix math to data arrays just like you do with numpy
+seas_anom = seas_mean - ann_mean.mean('year')
+
+#%%
+#Step 6 : plot the seasonal anomaly maps
+
+#This will plot all four xarray defined seasons (we can talk about 
+#selecting different seasons another time!) and plot them with the 
+#automatic xarray plotting function
+seas_anom.plot(col="season")
+plt.show()
+plt.clf()
+
+#But we can make nicer plots with geography (using cartopy)
+#Here we will just plot for JJA by using .sel and the new coordinate 
+#season with the label ‘JJA’
+#We can look at more ways to plot and the details below later on
+
+#---plot with two shapefile approaches (caropy and shapely, and gpd)
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines()
+ax.add_feature(cartopy.feature.BORDERS)
+ax.add_feature(cartopy.feature.RIVERS)
+
+#approach 1
+shape_feature = ShapelyFeature(Reader("./LakeTana_WGS/Lake_Tana_WGS.shp").geometries(),
+                                 ccrs.PlateCarree(), facecolor='none', edgecolor='black')
+ax.add_feature(shape_feature, edgecolor='green',facecolor='none',lw=5)
+
+#approach 2
+data = gpd.read_file('./LakeTana_WGS/Lake_Tana_WGS.shp')
+print(data.keys())
+print("tana crs", data.crs)
+data.plot(ax=ax, edgecolor='yellow', facecolor='none',lw=1,zorder=2,linestyle='--')
+
+#plot  data
+seas_anom.sel(season='JJA').plot(ax=ax,transform=ccrs.PlateCarree())
+# for extent the order is  [West,East,South,North]
+ax.set_extent([33, 47, 3.5, 14.5])
+plt.show()
+plt.clf()
+
+#---Approach to clip data using shapefiles
+rasterarray = xr.open_dataarray("./workshop_chirps.nc")
+rasterarray.rio.write_crs("epsg:4326", inplace=True)
+
+seas_anom = rasterarray.groupby("time.season").mean('time') - rasterarray.mean('time')
+
+data = gpd.read_file('/Users/ellendyer/Documents/Work/shapefiles/LakeTana_WGS/Lake_Tana_WGS.shp')
+new_seas_anom = seas_anom.rio.clip(data.geometry.apply(mapping),data.crs)
+                                 
+
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines()
+ax.add_feature(cartopy.feature.BORDERS)
+ax.add_feature(cartopy.feature.RIVERS)
+
+new_seas_anom.sel(season='JJA').plot(ax=ax,transform=ccrs.PlateCarree())
+# for extent the order is  [West,East,South,North]
+ax.set_extent([33, 47, 3.5, 14.5])
+plt.show()
+plt.clf()
+
+#%%
+#Step 7 : plot Awash maps
+
+#---another approach to clip
+rasterarray = xr.open_dataarray("./workshop_chirps.nc")
+rasterarray.rio.write_crs("epsg:4326", inplace=True)
+
+seas_anom = rasterarray.groupby("time.season").mean('time') - rasterarray.mean('time')
+
+river_data = gpd.read_file('./Awash/Awash_river_network.shp')
+print(river_data)
+
+
+data = gpd.read_file('./Awash/Awash_basin_border.shp')
+print(data)
+print(data.keys())
+print(data['OBJECTID'])
+print(data['BASIN_ID'])
+print(data['geometry'])
+print("awash crs", data.crs)
+new_seas_anom = seas_anom.rio.clip(data.geometry.apply(mapping),data.crs)
+                                 
+
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines()
+ax.add_feature(cartopy.feature.BORDERS)
+ax.add_feature(cartopy.feature.RIVERS)
+
+#there isn't a pure outline but you can select different
+#reional outlines from the shapefile like this
+#data = data.where(data['OBJECTID']==1)
+data.plot(ax=ax, edgecolor='darkorange', facecolor='none',lw=2,zorder=2,linestyle='-')
+
+#the rivers all seem like tiny segments so hard to select a good one
+#river_data = river_data.where(data['OBJECTID']==5)
+#river_data.plot(ax=ax, edgecolor='black', facecolor='none',lw=1,zorder=2,linestyle='-')
+
+new_seas_anom.sel(season='JJA').plot(ax=ax,transform=ccrs.PlateCarree())
+# for extent the order is  [West,East,South,North]
+ax.set_extent([37.9, 43.5, 7.7, 12.3])
+plt.show()
+plt.clf()
+
+#%%
+#Step 8 : plot Ethiopia maps
+
+#---another approach to clip
+rasterarray = xr.open_dataarray("./workshop_chirps.nc")
+rasterarray.rio.write_crs("epsg:4326", inplace=True)
+
+seas_anom = rasterarray.groupby("time.season").mean('time') - rasterarray.mean('time')
+
+
+data = gpd.read_file('./afr_g2014_2013_0/afr_g2014_2013_0.shp')
+print(data)
+print(data.keys())
+print(data['geometry'])
+print(data['ADM0_NAME'])
+print(data['ICPAC'])
+datanew=data[data['ADM0_NAME']=='Ethiopia']
+#datanew=data[data['IGAD']==4]
+print(datanew)
+print("countries crs", datanew.crs)
+new_seas_anom = seas_anom.rio.clip(datanew.geometry.apply(mapping),datanew.crs)
+                                 
+
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines()
+ax.add_feature(cartopy.feature.BORDERS)
+ax.add_feature(cartopy.feature.RIVERS)
+
+#there isn't a pure outline but you can select different
+#reional outlines from the shapefile like this
+#data_eth = data.where(data['ADM0_NAME']=='Ethiopia')
+data_eth = data.where(data['ICPAC']==14)
+data_eth.plot(ax=ax, edgecolor='green', facecolor='none',lw=2,zorder=2,linestyle='-')
+
+#the rivers all seem like tiny segments so hard to select a good one
+#river_data = river_data.where(data['OBJECTID']==5)
+#river_data.plot(ax=ax, edgecolor='black', facecolor='none',lw=1,zorder=2,linestyle='-')
+
+new_seas_anom.sel(season='JJA').plot(ax=ax,transform=ccrs.PlateCarree())
+# for extent the order is  [West,East,South,North]
+ax.set_extent([20, 50, -5, 17])
+plt.show()
+plt.clf()
+
+
+
+#%%
+#Step 9 : write a data array back to a netcdf file 
+
+# write seas_anom to netCDF to save your work 
+#for later or share your calculations
+#It's always a good idea to use a defined path!
+seas_anom.to_netcdf("./files/workshop_out_sa.nc")
+
---
+++ code/workshop_test_station_shape.py
@@ -0,0 +1,248 @@
+#Step 1: Start with import statements (this tells python which packages 
+#you will actually use in your code and names 
+#them, so we will call xarray : xr)
+#import sys
+import xarray as xr
+import pandas as pd
+import numpy as np
+#Import for Step 7
+from matplotlib import pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature
+#Import for Step 9
+#**To run this code you will need to conda install geopandas and rioxarray
+import geopandas as gpd
+from shapely.geometry import mapping
+
+#Here is a function definition - someone wrote this and shared it
+#to help other strip empty spaces from excel file fields
+#and we will 'call it' later in the code. Usually function definitions
+#are put at the start of the code.
+def trim_all_columns(df):
+    """
+    Trim whitespace from ends of each value across all series in dataframe
+    """
+    trim_strings = lambda x: x.strip() if isinstance(x, str) else x
+    return df.applymap(trim_strings)
+
+#%%
+##Step 2: Read in our data from a netcdf file of rainfall over Ethiopia
+#(edit the path to the file correctly depending on where you saved it) 
+#and select the variable ‘precip’. Now we have a data array called da and
+#we can print it to see what we have:
+da = xr.open_dataarray("./workshop_chirps.nc")
+#convert from mm/month to mm/day
+da = da/da.time.dt.daysinmonth
+da = da.sel(time=slice('1996-01-01','2010-12-31'))
+# print data array
+#print(da)
+
+#select a region and time slice from ts timeseries (you can use date strings
+#with xarray). The .sel slice method chooses the nearest point so
+#you don't need to know exact bounds
+chirps_reg = da.sel(lat=slice(7,12),lon=slice(36,40),time=slice('1990-01-01','2020-12-31'))
+#print(chirps_reg)
+
+#%%
+##Step 3: Read in station data from NMA
+
+#You can read in spreadsheet station data as a .csv file here:
+st = pd.read_csv("./NMA_Tana_basin.csv",header=0,index_col=False)
+
+#OR you can read in spreadsheet station data as an excel file here:
+# st = pd.read_excel("./NMA_Tana_basin.xlsx",header=0,index_col=False,sheet_name='data')
+#IF you read in an excel file you need to make sure all the column names are strings
+# st.columns = st.columns.map(str)
+
+#strip extra white space around entries
+st = trim_all_columns(st)
+#print head of the dataframe to see column names and values
+print(st.head())
+#Select just rainfall data (here called 'PRECIP')
+stpr = st[st['Elements'] == 'PRECIP']
+#drop any columns that aren't useful to your task
+stpr = stpr.drop(columns=['Stations','ID','Elements','Elivation','Monthly total'])
+#check the names of columns by printing the keys of the dataframe
+print(stpr.keys())
+
+#%%
+##Step 4: Start rearranging the dataframe so we can work with it as an xarray and also
+#so that we can put in datetimes (useful later on)
+#melt moves around the headers so that days are now a column
+newst = pd.melt(stpr,id_vars=['Years', 'Latitiude', 'Longitiude', 'Month', 'Time'], value_vars=np.arange(1,32).astype('str'))
+#Can rename columns so that they have generic names
+newst = newst.rename(columns={"variable": "Day", "value": "PRECIP","Latitiude":"lat", "Longitiude":"lon"})
+#print(newst.head())
+#Drop any rows that don't have values (because the month doesn't have 31 days for instance)
+newst = newst.dropna(axis=0)
+#Make a list of dates for all values in the dataframe - could also include the hour
+date = pd.to_datetime(dict(year=newst.Years, month=newst.Month, day=newst.Day), errors='coerce')#,hour=newst.Time))
+#Add this list of datetimes to the dataframe
+newst['time'] = date.values
+#Now we no longer need the columns Years, Month, Day because we made a datetime with them
+newst = newst.drop(columns=['Years','Month','Day'])
+#Rearrange the columns in the order that makes sense for the work
+newst = newst[['time','lat','lon','PRECIP']]
+#print(newst.head())
+#You can also sort values, here we did it by time, then lat, then lon
+newst.sort_values(by=['time','lat','lon'],inplace=True)
+#Some values are still 'empty' ie there may have been an entry registered
+#but there was no readable data, so we replace those '' with nans so we
+#can then drop the nans
+newst = newst.replace('', np.nan)
+newst = newst.dropna(axis=0)
+#Because PRECIP is the variable we want to later plot
+#we want to make sure all the values are marked as floats
+newst = newst.astype({'PRECIP': 'float64'})
+#Now if we want to turn this into an xarray with PRECIP as the variable
+#we can set time, lat, and lon as index values
+newst_toxr = newst.set_index(['time','lat','lon'])
+#Select same date range as chirps
+newst_toxr = newst_toxr.loc['1996-01-01':'2010-12-31']
+print(newst_toxr.head())
+
+#%%
+## Step 5: Create an xarray or output the dataframe to a file
+#Now the dataframe is setup to be easily converted to an xarray that
+#we can make a spatial plot of
+xrst = newst_toxr.to_xarray()['PRECIP']
+#dropping all dimensions with all nan values because xarray has made a mesh
+xrst = xrst.dropna('lat','all')
+xrst = xrst.dropna('lon','all')
+#xrst.mean('time', skipna=True).plot()
+#plt.show()
+#plt.clf()
+xrst.to_netcdf('./files/out.nc')
+#We can also output our dataframe in this new
+#organisation to a csv file (other formats available)
+newst.to_csv('./files/out.csv')
+
+
+#%%
+## Step 6 : Select some months from both the CHIRPS and NMA datasets
+# Here we will look at March-May
+months = [3,4,5]
+da_mam = da.sel(time=np.in1d(da['time.month'], months)).mean('time')
+xrst_mam = xrst.sel(time=np.in1d(xrst['time.month'], months)).mean('time')
+newst_mam = newst_toxr.loc[(newst_toxr.index.get_level_values('time').month.isin([3,4,5]))].groupby(level=('lat','lon')).mean()
+#print(newst_mam.index.get_level_values('lat').values)
+#print(newst_mam.head(15))
+
+#%%
+## Step 7: Plot chirps as a background with a station scatter from both the xarray and pandas dataframe
+
+#Create an empty figure - the size is (width,height) and you might have
+#to adjust this later
+fig = plt.figure(figsize=(8,5))
+#Create an axis for your figure and you can use a coordinate projection
+#
+ax = plt.axes(projection=ccrs.PlateCarree())
+#Do a filled contour with chirps (same as first example)
+#alpha of less than 1 makes the contours a bit transparent so its easier to see the scatter
+da_mam.plot.contourf(ax=ax,transform=ccrs.PlateCarree(),alpha=0.8,vmin=0,vmax=4,
+                     cbar_kwargs={'label': "MAM CHIRPS contours (mm/day)"})
+#Create a mesh of lat lons using the xarray of station data we made above
+#This is necessary because the xarray is a grid instead of a list
+lon, lat = np.meshgrid(xrst_mam.lon,xrst_mam.lat)
+xrst_p = ax.scatter(x=lon,y=lat,s=60,c=xrst_mam,vmin=0,vmax=4,
+                    edgecolors=None,transform=ccrs.PlateCarree())
+plt.colorbar(xrst_p,label='MAM Station xarray (mm/day)',ax=ax)
+#For a scatter from the dataframe we need to select the index values for lat and lon
+#Because there are multiple indices (time, lat, lon) we need to use get_level_values
+#because these indices are called values
+newst_p = ax.scatter(x=newst_mam.index.get_level_values('lon').values,
+                     y=newst_mam.index.get_level_values('lat').values,
+                     s=60,c=newst_mam['PRECIP'].values,vmin=0,vmax=4,
+                     edgecolors='black',transform=ccrs.PlateCarree())
+plt.colorbar(newst_p,label='MAM Station dataframe (mm/day)',ax=ax)
+#*** You can't see both scatters because they are on top of each other - comment each one
+#    out at a time and plot to see if you can see a difference in them
+#Now we add all the cartopy information we did last time and add in some gridlines so it
+#is easier to read the plot
+ax.coastlines()
+ax.add_feature(cartopy.feature.BORDERS)
+ax.add_feature(cartopy.feature.RIVERS)
+gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                  linewidth=1, color='black', alpha=0.5, linestyle='dotted')
+gl.top_labels = False
+gl.right_labels = False
+# for extent the order is  [West,East,South,North]
+ax.set_extent([36, 38.5, 9, 13.5])
+plt.title('Station scatter (pandas and xarray) \n over CHIRPS rainfall contours \n in MAM')
+plt.savefig('./plots/plot1.png', bbox_inches='tight',dpi=200)
+plt.show()
+plt.clf()
+
+#%%
+## Step 8: Interpolate Chirps data to the station locations so we can then calculate the difference
+#between values at station locations
+#There are options for interpolation methods but I have chosen to linearly interpolate here
+da_st = da.interp(lon=xrst.lon.values,lat=xrst.lat.values,method='linear')
+diff_st = da_st - xrst
+diff_st = diff_st.mean('time')
+#print(diff_st)
+
+fig = plt.figure(figsize=(7,6))
+ax = plt.axes(projection=ccrs.PlateCarree())
+#Do a filled contour with chirps (same as first example)
+#alpha of less than 1 makes the contours a bit transparent so its easier to see the scatter
+da.mean('time').plot.pcolormesh(cmap=plt.cm.viridis,ax=ax,transform=ccrs.PlateCarree(),
+                                alpha=1.0,cbar_kwargs={'label': "Mean CHIRPS mesh (mm/day)"})
+#Create a mesh of lat lons using the xarray of station data we made above
+#This is necessary because the xarray is a grid instead of a list
+lon, lat = np.meshgrid(diff_st.lon,diff_st.lat)
+diffp = ax.scatter(x=lon,y=lat,s=60,c=diff_st,vmin=-1.5,vmax=1.5,
+                   cmap=plt.cm.PRGn,edgecolors=None,transform=ccrs.PlateCarree())
+plt.colorbar(diffp,label='Mean CHIRPS-station (mm/day)',ax=ax)
+ax.coastlines()
+ax.add_feature(cartopy.feature.BORDERS)
+ax.add_feature(cartopy.feature.RIVERS)
+gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                  linewidth=1, color='black', alpha=0.5, linestyle='dotted')
+gl.top_labels = False
+gl.right_labels = False
+ax.set_extent([36, 38.5, 9, 13.5])
+plt.title('Difference between interpoloated \n CHIRPS and station '
+          'data \n over CHIRPS rainfall mesh \n for long term mean')
+plt.savefig('./plots/plot2.png', bbox_inches='tight',dpi=200)
+plt.show()
+plt.clf()
+
+
+#%%
+## Step 9: Try to cut out this data for Tana basin where the NMA station data is located
+
+#Read in your shapefile using a library called geopandas - we called it gpd at the start of the code
+shape = gpd.read_file('/Users/ellendyer/Documents/Work/REACH/Workshop_conda_python/LakeTana_WGS/Lake_Tana_WGS.shp')
+#Now you have to add some coordinate information to your CHIRPS datarray (in this case epsg:4326)
+da_rio = da.rio.write_crs("epsg:4326", inplace=True)
+#Now that the shapefile and dataframe have coordinates you can use rio.clip to cut the dataarray out in
+#using the shapefile
+da_clip = da.rio.clip(shape.geometry.apply(mapping), shape.crs)
+
+fig = plt.figure(figsize=(7,6))
+ax = plt.axes(projection=ccrs.PlateCarree())
+#Do a filled contour with chirps (same as first example)
+#alpha of less than 1 makes the contours a bit transparent so its easier to see the scatter
+da_clip.mean('time').plot.pcolormesh(cmap=plt.cm.viridis,ax=ax,transform=ccrs.PlateCarree(),
+                                alpha=1.0,cbar_kwargs={'label': "Mean CHIRPS mesh (mm/day)"})
+shape.plot(ax=ax, edgecolor='black', facecolor='none',lw=2,zorder=2,linestyle='--')
+#Create a mesh of lat lons using the xarray of station data we made above
+#This is necessary because the xarray is a grid instead of a list
+lon, lat = np.meshgrid(diff_st.lon,diff_st.lat)
+diffp = ax.scatter(x=lon,y=lat,s=60,c=diff_st,vmin=-1.5,vmax=1.5,
+                   cmap=plt.cm.PRGn,edgecolors=None,transform=ccrs.PlateCarree())
+plt.colorbar(diffp,label='Mean CHIRPS-station (mm/day)')
+ax.coastlines()
+ax.add_feature(cartopy.feature.BORDERS)
+ax.add_feature(cartopy.feature.RIVERS)
+gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                  linewidth=1, color='black', alpha=0.5, linestyle='dotted')
+gl.top_labels = False
+gl.right_labels = False
+ax.set_extent([36, 38.5, 9, 13.5])
+plt.title('Difference between interpoloated \n CHIRPS and station '
+          'data \n over CHIRPS rainfall mesh \n for long term mean')
+plt.savefig('./plots/plot3.png', bbox_inches='tight',dpi=200)
+plt.show()
+plt.clf()
